d51435910a40c033c79c4a8f34773d9b9de2a63a
Christopher Jung <christopher.h-jung@web.de>
2021-12-13 12:12:42 +0100

    use buid runtime from buld folder

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 . ../project.sh
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../runtime/build/lib
 impala test.impala --emit-thorin -o out &&
 impala test.impala --emit-llvm -o out &&
 llvm-as out.ll &&
-clang -L../runtime/build/lib/ -lruntime -lm lib.c out.bc -o prog &&
+clang -L../runtime/build/lib -lruntime -lm lib.c out.bc -o prog &&
 ./prog
