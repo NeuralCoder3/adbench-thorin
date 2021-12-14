1c87f5dd0c8ce6bb6765abab377252eac52b5d20
Christopher Jung <christopher.hendrik.jung@gmail.com>
2021-12-14 15:26:15 +0100

    move implementation into impala folder

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 . ../project.sh
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../runtime/build/lib
-impala gmm.impala --emit-thorin -o build/out &&
-impala gmm.impala --emit-llvm -o build/out &&
+impala impala/gmm.impala --emit-thorin -o build/out &&
+impala impala/gmm.impala --emit-llvm -o build/out &&
 llvm-as build/out.ll &&
-clang -L../runtime/build/lib -lruntime -lm lib.c build/out.bc -o build/prog &&
+clang -L../runtime/build/lib -lruntime -lm impala/lib.c build/out.bc -o build/prog &&
 ./build/prog
