798224f01858bfea8d657d3a5ac0498d3b4120f5
Christopher Jung <christopher.hendrik.jung@gmail.com>
2021-12-14 16:24:28 +0100

    implement reference derivative

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,25 +9,40 @@
 
 
 int main(){
+    int d;
+    int k;
+    int n;
+    vector<double> alphas;
+    vector<double> means;
+    vector<double> icf;
+    vector<double> x;
+    Wishart wishart;
 
-  int d;
-  int k;
-  int n;
-  vector<double> alphas;
-  vector<double> means;
-  vector<double> icf;
-  vector<double> x;
-  Wishart wishart;
+    std::string benchmark = "benchmark/gmm_d2_K5.txt";
+    read_gmm_instance(benchmark, &d, &k, &n, alphas, means, icf, x, wishart, false);
 
-  std::string benchmark = "benchmark/gmm_d2_K5.txt";
-  read_gmm_instance(benchmark, &d, &k, &n, alphas, means, icf, x, wishart, false);
+    //cpp : -5240.590563
+    //impala : -5240.589720
+    //diff: 0.000843
 
-  //cpp : -5240.590563
-  //impala : -5240.589720
-  //diff: 0.000843
+    double error = 0;
+    gmm_objective<double>(d, k, n, &alphas[0], &means[0], &icf[0], &x[0], wishart, &error);
+    printf("%lf\n", error);
 
-  double error = 0;
-  gmm_objective<double>(d, k, n, &alphas[0], &means[0], &icf[0], &x[0], wishart, &error);
-  printf("%lf", error);
-  return 0;
+    vector<double> J;
+
+
+    int icf_sz = d * (d + 1) / 2;
+    J.resize(k + d * k + icf_sz * k);
+    gmm_objective_d(d, k, n, &alphas[0], &means[0], &icf[0], &x[0], wishart, &error, &J[0]);
+
+    printf("\n");
+
+
+    for (int j = 0; j < J.size(); j++)
+    {
+        printf("%lf\n", J[j]);
+    }
+
+    return 0;
 }
