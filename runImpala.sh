27ec0b924bab1d263d9fe1d97ebfd3a2f7065036
Christopher Jung <christopher.hendrik.jung@gmail.com>
2021-12-14 16:15:18 +0100

    move impala to build/impala build location

--- runImpala.sh
+++ runImpala.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 . ../project.sh
+mkdir -p build/impala
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../runtime/build/lib
-impala impala/gmm.impala --emit-thorin -o build/out &&
-impala impala/gmm.impala --emit-llvm -o build/out &&
-llvm-as build/out.ll &&
-clang -L../runtime/build/lib -lruntime -lm impala/lib.c build/out.bc -o build/prog &&
-./build/prog
+impala impala/gmm.impala --emit-thorin -o build/impala/out &&
+impala impala/gmm.impala --emit-llvm -o build/impala/out &&
+llvm-as build/impala/out.ll &&
+clang -L../runtime/build/lib -lruntime -lm impala/lib.c build/impala/out.bc -o build/impala/prog &&
+./build/impala/prog
